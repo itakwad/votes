bf234482f628cfe510c7d5d617326a7a1e3f1baf
itakwad <136983253+itakwad@users.noreply.github.com>
2024-01-18 22:03:15 +0200

    Create users_insert.sql

---
+++ users_insert.sql
@@ -0,0 +1,14 @@
+
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `img_url`, `is_admin`, `preferences`, `created_at`) VALUES
+(2, 'احمد علي', 'fermin51@example.com', '$2y$11$HQfkHHNpyRLBwMkfLm561Obo7rhpzvR9pNW9VY1eCtFymwXADI8i6', '2_img.jpg', 0, 'a:6:{i:0;s:1:\"1\";i:1;s:1:\"2\";i:2;s:1:\"3\";i:3;s:1:\"4\";i:4;s:1:\"5\";i:5;s:1:\"6\";}', '2024-01-07 19:34:49'),
+(3, 'مستخدم1', 'user1@example.com', 'hashed_password1', 'img1.jpg', 0, 'a:1:{i:0;s:1:\"1\";}', '2024-01-18 14:27:57'),
+(4, 'مستخدم2', 'user2@example.com', 'hashed_password2', 'img2.jpg', 0, 'a:2:{i:0;s:1:\"1\";i:1;s:1:\"2\";}', '2024-01-18 14:27:57'),
+(5, 'مستخدم3', 'user3@example.com', 'hashed_password3', 'img3.jpg', 0, 'a:3:{i:0;s:1:\"1\";i:1;s:1:\"2\";i:2;s:1:\"3\";}', '2024-01-18 14:27:57'),
+(6, 'مستخدم4', 'user4@example.com', 'hashed_password4', 'img4.jpg', 0, 'a:4:{i:0;s:1:\"1\";i:1;s:1:\"2\";i:2;s:1:\"3\";i:3;s:1:\"4\";}', '2024-01-18 14:27:57'),
+(7, 'مستخدم5', 'user5@example.com', 'hashed_password5', 'img5.jpg', 0, 'a:5:{i:0;s:1:\"1\";i:1;s:1:\"2\";i:2;s:1:\"3\";i:3;s:1:\"4\";i:4;s:1:\"5\";}', '2024-01-18 14:27:57'),
+(8, 'مستخدم6', 'user6@example.com', 'hashed_password6', 'img6.jpg', 0, 'a:6:{i:0;s:1:\"1\";i:1;s:1:\"2\";i:2;s:1:\"3\";i:3;s:1:\"4\";i:4;s:1:\"5\";i:5;s:1:\"6\";}', '2024-01-18 14:27:57'),
+(9, 'مستخدم7', 'user7@example.com', 'hashed_password7', 'img7.jpg', 0, 'a:1:{i:0;s:1:\"1\";}', '2024-01-18 14:27:57'),
+(10, 'مستخدم8', 'user8@example.com', 'hashed_password8', 'img8.jpg', 0, 'a:2:{i:0;s:1:\"1\";i:1;s:1:\"2\";}', '2024-01-18 14:27:57'),
+(11, 'مستخدم9', 'user9@example.com', 'hashed_password9', 'img9.jpg', 0, 'a:3:{i:0;s:1:\"1\";i:1;s:1:\"2\";i:2;s:1:\"3\";}', '2024-01-18 14:27:57'),
+(12, 'مستخدم10', 'user10@example.com', 'hashed_password10', 'img10.jpg', 0, 'a:4:{i:0;s:1:\"1\";i:1;s:1:\"2\";i:2;s:1:\"3\";i:3;s:1:\"4\";}', '2024-01-18 14:27:57');
